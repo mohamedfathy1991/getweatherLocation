f151c38b425ede3661d3e6e08a2e514ab5a3d18d
mohamed fathy <mohamed.electronics1991@gmail.com>
2024-01-11 15:19:39 +0200

    add  weather for two days the next day and after tomorrow

--- index.js
+++ index.js
@@ -3,14 +3,24 @@ let monthDay = document.querySelector("#monthday");
 let zone = document.querySelector("#lcationname");
 let temperature = document.querySelector("#temp-c");
 let weatherCondition = document.querySelector("#weather-condition");
+let imageweather = document.querySelector("#imgageweather");
+let nextdayName= document.getElementsByClassName('next-day')
+let nextCondtionType= document.getElementsByClassName('next-condition-type')
+let nextDayTemp= document.getElementsByClassName('next-cond-temp')
+let nextDayIcon= document.getElementsByClassName('next-condtion-icon')
 
+
+
+
+// fetch data for weather for three days f
 async function weather(val) {
   try {
     const weatherData = await fetch(
-      `https://api.weatherapi.com/v1/current.json?key=c1830b1a56ff4a03be8191253240301&q=${val}`
+      `https://api.weatherapi.com/v1/forecast.json?key=c1830b1a56ff4a03be8191253240301&q=${val}&days=3`
     );
 
     let data = await weatherData.json();
+    console.log(data)
 
     let weakdayName = new Date(data.current.last_updated).toLocaleString(
       "en-us",
@@ -20,14 +30,38 @@ async function weather(val) {
       "en-us",
       { month: "long" }
     );
-    let dayDigit = new Date(data.current.last_updated).getDay();
+    let dayDigit = new Date(data.current.last_updated).getDate()
+    
 
     zone.innerHTML = `${data.location.name}`;
     temperature.innerHTML = `${data.current.temp_c}c`;
+
     weatherCondition.innerHTML = `  ${data.current.condition.text}`;
+
+     imageweather.src=`https:${data.current.condition.icon}`
+  
     day.innerHTML = weakdayName;
     monthDay.innerHTML = dayDigit;
     monthDay.innerHTML += monthName;
+
+
+
+    // print data for the next 2 days
+
+    for(let i=1;i<3;i++){
+      // name day of tomorrow and after tomorrow
+      nextdayName[i-1].innerHTML= new Date( data.forecast.forecastday[i].date).toLocaleString(
+        "en-us",
+        { weekday: "long" })
+        nextDayTemp[i-1].innerHTML=`${data.forecast.forecastday[i].day.maxtemp_c} c <br>
+        ${data.forecast.forecastday[i].day.mintemp_c}c`
+        nextCondtionType[i-1].innerHTML=data.forecast.forecastday[i].day.condition.text
+        nextDayIcon[i-1].src=`https:${data.forecast.forecastday[i].day.condition.icon}`
+
+        
+    }
+
+
   } catch (err) {
     console.log(err);
   }
@@ -53,6 +87,8 @@ async function getweather(value) {
   }
 }
 
+
+// to get current position and then get weather for position
 function getLocation() {
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(showPosition);
@@ -61,6 +97,8 @@ function getLocation() {
   }
 }
 
+// get weather after get the current  location 
 function showPosition(position) {
   weather(`${position.coords.latitude},${position.coords.longitude}`);
 }
+
